02b34aa1e298fe0589bb194bd440bf47b6bd8138
antonio isaac ramirez monsalve <metal_invaders_21@hotmail.com>
2020-05-02 22:48:30 -0400

    Agregar archivos de proyecto.

---
+++ POO/Bicicleta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO
+{
+    class Bicicleta
+    {
+        public string marca;
+        public string modelo;
+        public int rodada;
+        public int velocidades;
+        public string color;
+
+
+        public void Acelerar()
+        {
+            Console.WriteLine("La bicicleta ha empezado a rodar!!");
+        }
+
+        public void frenar()
+        {
+            Console.WriteLine("La bicicleta se detuvo");
+        }
+
+        public void CambioVelocidades()
+        {
+            Console.WriteLine("La bicicleta cambio su velocidad");
+        }
+
+        public void informacion()
+        {
+            Console.WriteLine("Informacion de la bicicleta: ");
+            Console.WriteLine("Marca: "+marca);
+            Console.WriteLine("Modelo: "+ modelo);
+            Console.WriteLine("rodada:"+ rodada);
+            Console.WriteLine("velocidades:"+ velocidades);
+            Console.WriteLine("color: "+ color);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //instancia de la clase
+            Bicicleta Bicicleta1 = new Bicicleta();
+            //asignacion de valores a los campos
+            Bicicleta1.marca = "Factor";
+            Bicicleta1.modelo = "X45";
+            Bicicleta1.color = "Roja";
+            Bicicleta1.rodada = 23;
+            Bicicleta1.velocidades = 7;
+
+            Bicicleta1.Acelerar();
+            Bicicleta1.informacion();
+
+
+            Console.ReadKey();
+        } 
+
+    }
+}
