a6a4c5adf1e910d6c72ac72b449b3d7d1b9f6ab4
antonio isaac ramirez monsalve <metal_invaders_21@hotmail.com>
2020-05-03 00:43:39 -0400

    commit1Calculadora

---
+++ POO/Calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace POO
+{
+    class Calculadora
+    {
+        public double num1;
+        public double num2;
+        public double resultado;
+        public int opcion = 0;
+        
+
+        public void Suma()
+        {
+            resultado = num1 + num2;
+            Console.WriteLine(num1 + "+" + num2 + "=" + resultado);
+        }
+
+        public void Resta()
+        {
+            resultado = num1 - num2;
+            Console.WriteLine(num1 + "-" + num2 + "=" + resultado);
+        }
+
+        public void Multiplicar()
+        {
+            resultado = num1 * num2;
+            Console.WriteLine(num1 + "-" + num2 + "=" + resultado);
+        }
+
+        public void dividir()
+        {
+            if (num1 == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir por 0");
+            }
+            else
+            {
+                resultado = num1 / num2;
+                Console.WriteLine(num1 + "/" + num2 + "=" + resultado);
+            }
+
+            
+        }
+    }
+}
--- POO/Program.cs
+++ POO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace POO
 {
@@ -7,20 +8,67 @@ namespace POO
 
         static void Main(string[] args)
         {
-            //instancia de la clase
-            Bicicleta Bicicleta1 = new Bicicleta();
-            //asignacion de valores a los campos
-            Bicicleta1.marca = "Factor";
-            Bicicleta1.modelo = "X45";
-            Bicicleta1.color = "Roja";
-            Bicicleta1.rodada = 23;
-            Bicicleta1.velocidades = 7;
+            ////instancia de la clase
+            //Bicicleta Bicicleta1 = new Bicicleta();
+            ////asignacion de valores a los campos
+            //Console.WriteLine("Primera bicicleta");
+            //Bicicleta1.marca = "Factor";
+            //Bicicleta1.modelo = "X45";
+            //Bicicleta1.color = "Roja";
+            //Bicicleta1.rodada = 23;
+            //Bicicleta1.velocidades = 7;
+            //Bicicleta1.Acelerar();
+            //Bicicleta1.informacion();
 
-            Bicicleta1.Acelerar();
-            Bicicleta1.informacion();
 
 
-            Console.ReadKey();
+
+            //Console.WriteLine("\n\nSegunda bicicleta");
+            //Bicicleta bicicleta2 = new Bicicleta();
+            //Bicicleta1.marca = "BIANCHI";
+            //Bicicleta1.modelo = "j78";
+            //Bicicleta1.color = "NEGRO";
+            //Bicicleta1.rodada = 22;
+            //Bicicleta1.velocidades = 10;
+
+            //bicicleta2.informacion();
+            //Console.WriteLine("\n\nPulse cualquier tecla");
+            //Console.ReadKey();
+
+
+            Calculadora calculadora1 = new Calculadora();
+            Console.WriteLine("Ingresa numero");
+            calculadora1.num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa numero");
+            calculadora1.num2 = Convert.ToDouble(Console.ReadLine());
+           
+            while ((calculadora1.opcion > 0 && calculadora1.opcion < 4 )==false) 
+            {
+                Console.WriteLine("Ingrese opcion 1: para  sumar\nIngrese opcion 2: para restar\nIngrese opcion3 Para Multiplicar\nIngrese opcion 4: para dividir");
+                calculadora1.opcion = Convert.ToInt32(Console.ReadLine());
+
+            }
+       
+
+            if (calculadora1.opcion == 1)
+            {
+
+                calculadora1.Suma();
+            }
+            if (calculadora1.opcion == 2)
+            {
+                calculadora1.Resta();
+            }
+            if (calculadora1.opcion == 3)
+            {
+                calculadora1.Multiplicar();
+            }
+            if (calculadora1.opcion == 4)
+            {
+                calculadora1.dividir();
+            }
+           
+
         } 
 
     }
